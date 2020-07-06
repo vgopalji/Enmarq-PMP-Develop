226ac5f36d74f0d86a3291a4b27b8d6b9c40ea65
vgopalji <vgopalji@hotmail.com>
2020-07-06 18:21:47 +0530

    Update Startup.cs

--- CareStream.WebApp/Startup.cs
+++ CareStream.WebApp/Startup.cs
@@ -86,6 +86,7 @@ namespace CareStream.WebApp
                 app.UseStatusCodePagesWithReExecute("/Error/{0}");
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
