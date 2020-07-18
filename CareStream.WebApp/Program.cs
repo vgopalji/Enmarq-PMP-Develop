3c1bcdcaf7224ae95649003e8451e233109feedc
vgopalji <vgopalji@hotmail.com>
2020-07-18 14:58:20 +0530

    Latest

--- CareStream.WebApp/Program.cs
+++ CareStream.WebApp/Program.cs
@@ -16,7 +16,7 @@ namespace CareStream.WebApp
         {
             CreateHostBuilder(args).Build().Run();
         }
-
+         
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
